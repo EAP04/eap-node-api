2e18410a9c4f74ac9f3f885a8917111e82cdeb9d
Edivaldo Amane Pedro <edivaldoamanepedro@gmail.com>
2022-05-22 03:26:35 +0200

    Initial commit

---
+++ eap2.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var server = http.createServer(function (req, res) {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  if (req.url == "/") {
+    res.write("<h1>Pagina Inicial</h1>");
+  } else if (req.url == "/bemvindo") {
+    res.write("<h1>Bem vindo</h1>");
+  } else {
+    res.write("<h1>Pagina nao encontrado</h1>");
+  }
+  res.end();
+});
+server.listen(3000, function () {
+  console.log("Servidor ligado");
+});
